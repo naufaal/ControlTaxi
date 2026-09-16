6aca4d4c000ad2aeb8a67a2db2bddeb4a86552ad
naufaal <naufaal@outlook.com>
2026-09-16 01:51:03 +0000

    2

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.controltaxi.app',
-  appName: 'ControlTaxi',
-  webDir: '.output/public', // <-- Cambia 'build' por '.output/public'
-};
-
-export default config;
